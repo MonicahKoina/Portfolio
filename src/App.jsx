e52df7de0992c8a22f3d9eccfa876c814e75af30
Monicah Koina <monicahkoina1@gmail.com>
2024-10-29 23:23:43 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+import React from 'react'
+function App() {
+ 
+
+  return (
+    <>
+    <div className='w-3/4 bg-black'>
+      <h1>hello</h1></div>
+    </>
+  )
+}
+export default App
